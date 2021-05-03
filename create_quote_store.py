ea0ac1486b2350982039fd4a57cec21ab9cef2f9
Juha-Samuli Hellen <juha-samuli.hellen@taaleri.com>
2021-05-03 21:31:29 +0300

    first

---
+++ create_quote_store.py
@@ -0,0 +1,51 @@
+import json
+import pandas as pd
+from bitmex_s3.src.downloader import (
+    QuoteData
+)
+
+def create_date_strings(start_date, end_date):
+
+    def month_str(month):
+        if month < 10:
+            return f"0{month}"
+        else:
+            return f"{month}"
+
+    def day_str(day):
+        if day < 10:
+            return f"0{day}"
+        else:
+            return f"{day}"
+
+    if end_date <= start_date:
+        raise ValueError("Check dates")
+
+    range = pd.date_range(start=start_date, end=end_date, freq='D')
+
+    dates = []
+    for date in range:
+        dates.append(f"{date.year}{month_str(date.month)}{day_str(date.day)}")
+
+    return dates
+
+
+def main():
+
+    with open('store.config.json', 'r') as json_file:
+        config = json.load(json_file)
+
+    bucket_name = config["bucket-name"]
+    data_type = config["data-type"]
+    quote_data = QuoteData(bucket_name)
+
+    store = pd.HDFStore(f"store.{data_type}/{bucket_name}.h5")
+    dates = create_date_strings(config["start-date"], config["end-date"])
+    for date in dates:
+        data = quote_data.get(date)
+        store.put(f"{data_type}_{date}", data, format='table')
+        print("Downloaded: ", date)
+
+
+if __name__ == '__main__':
+    main()
